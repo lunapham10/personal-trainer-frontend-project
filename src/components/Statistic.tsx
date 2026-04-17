da366208bec3e12b6a5fd10778c5b9e3923cfe90
Oanh Pham <oanh.pham@myy.haaga-helia.fi>
2026-04-17 16:56:55 +0300

    feat: add horizontal chart into statistic page

--- src/components/Statistic.tsx
+++ src/components/Statistic.tsx
@@ -2,14 +2,22 @@ import { BarChart } from '@mui/x-charts/BarChart';
 import { useState, useEffect } from "react";
 import type { Training } from '../types';
 import _ from "lodash";
+import type { HotPayload } from 'vite/types/hmrPayload.js';
+import { Box, Stack } from '@mui/system';
 
 type ChartData = {
     chartActivity: string;
     trainingDuration: number;
 }
 
+type ChartData2 = {
+    trainingPerson: string;
+    trainingDuration: number;
+}
+
 export default function Statistic() {
     const [chartData, setChartData] = useState<ChartData[]>([]);
+    const [horizontalChartData, setHorizontalChartData] = useState<ChartData2[]>([]);
     const fetchData = () => {
         fetch(import.meta.env.VITE_API_URL + "/gettrainings")
             .then(response => {
@@ -18,14 +26,23 @@ export default function Statistic() {
                 return response.json();
             })
             .then((data: Training[]) => {
-
+                //Activity Statistics chart
                 const groupedData = _.groupBy(data, 'activity');
-
                 const dataset: ChartData[] = _.map(groupedData, (items, key) => ({
                     chartActivity: key,
                     trainingDuration: _.sumBy(items, (t) => Number(t.duration))
                 }));
                 setChartData(dataset);
+
+                //Customer Training Volume chart
+                const groupedData2 = _.groupBy(data, (t) => {
+                    return t.customer ? `${t.customer.firstname} ${t.customer.lastname}` : 'Unknown';
+                });
+                const dataset2: ChartData2[] = _.map(groupedData2, (items, key) => ({
+                    trainingPerson: key,
+                    trainingDuration: _.sumBy(items, (t) => Number(t.duration))
+                }));
+                setHorizontalChartData(dataset2);
             })
             .catch(err => console.error(err));
     }
@@ -34,26 +51,33 @@ export default function Statistic() {
         fetchData();
     }, []);
 
-    const chartSetting = {
-        yAxis: [
-            { label: 'Duration (min)' },
-        ],
-        series: [
-            { dataKey: 'trainingDuration', label: 'chartActivity', color: '#1976d2' }
-        ],
-        height: 400,
-    };
+    const commonSeries = [{ dataKey: 'trainingDuration', label: 'Total Minutes', color: '#1976d2' }];
+
 
     return (
-        <div style={{ marginTop: '40px', width: '100%', display: 'flex', justifyContent: 'center' }}>
-            <div style={{ width: '80%' }}>
-                <h2 style={{ textAlign: 'center' }}>Activity Statistics</h2>
-                <BarChart
-                    dataset={chartData}
-                    xAxis={[{ scaleType: 'band', dataKey: 'chartActivity' }]}
-                    {...chartSetting}
-                />
-            </div>
-        </div>
+        <Box style={{ marginTop: '20px', width: '100%', display: 'flex', justifyContent: 'center' }}>
+            <Box style={{ width: '100%' }}>
+                <Stack spacing={3}>
+                    <h4 style={{ textAlign: 'center' }}>Activity Statistics</h4>
+                    <BarChart
+                        dataset={chartData}
+                        xAxis={[{ scaleType: 'band', dataKey: 'chartActivity' }]}
+                        yAxis={[{ label: 'Duration (min)' }]}
+                        series={commonSeries}
+                        height={350}
+                    />
+                    <h4 style={{ textAlign: 'center' }}>Customer Training Volume</h4>
+                    <BarChart
+                        layout="horizontal"
+                        dataset={horizontalChartData}
+                        yAxis={[{ scaleType: 'band', dataKey: 'trainingPerson' }]}
+                        xAxis={[{ label: 'Duration (min)' }]}
+                        series={commonSeries}
+                        height={350}
+                    />
+                </Stack>
+            </Box>
+        </Box>
+
     );
 }
